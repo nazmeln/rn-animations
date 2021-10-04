abedf033026df771f12b8343b03ee4c57bada89f
Nazarii Melnyk <nazarii.melnyk@rolique.io>
2021-10-04 19:49:51 +0300

    [ADD] navigation

---
+++ src/navigators/StackNavigator.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {FlatListAnimation} from '../animations';
+import {Home} from '../screens';
+import {ROUTES} from '../constants';
+
+const Stack = createStackNavigator();
+
+export const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name={ROUTES.HOME} component={Home} />
+      <Stack.Screen
+        name={ROUTES.FLAT_LIST_ANIMATIONS}
+        component={FlatListAnimation}
+      />
+    </Stack.Navigator>
+  );
+};
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import {Button, SafeAreaView, ScrollView, Text, View} from 'react-native';
+import {Button, SafeAreaView, ScrollView} from 'react-native';
+import {ROUTES} from '../../constants';
+
+import {NativeModules} from 'react-native';
+
+console.log('NativeModules', NativeModules.RNShare);
 
 import styles from './styles';
 
-export const Home = () => {
+export const Home = ({navigation}) => {
   return (
     <ScrollView contentInsetAdjustmentBehavior="automatic">
       <SafeAreaView style={styles.container}>
-        <Button title="Carousel Movie Animation" onPress={() => {}} />
+        <Button
+          title="Carousel Movie Animation"
+          onPress={() => navigation.navigate(ROUTES.FLAT_LIST_ANIMATIONS)}
+        />
       </SafeAreaView>
     </ScrollView>
   );
